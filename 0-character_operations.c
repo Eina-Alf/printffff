73a6766ae0de6ba34cb1a858bc4fd115557b11b0
Eina-Alf <Eina-Alf>
2022-11-21 14:10:44 -0800

    t

--- 0-character_operations.c
+++ 0-character_operations.c
@@ -1,11 +1,10 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * print_c - Print characters (%c)
  * @ar_list: Store the a list of characters
  * Return: num of parameters printed
  */
-
 int print_c(va_list ar_list)
 {
 	int c = va_arg(ar_list, int);
@@ -18,7 +17,6 @@ int print_c(va_list ar_list)
 * @ar_list: Store the a list of characters
 * Return: num of parameters printed
 */
-
 int print_s(va_list ar_list)
 {
 	int i, count = 0;
